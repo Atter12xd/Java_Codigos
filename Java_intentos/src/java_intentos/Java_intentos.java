7c4323c35ed6348aa2366fcbb040fc7e3985f19f
Atter12xd <1485275@senati.pe>
2023-03-10 23:35:29 -0500

    Primeros codigos con Java en NetBeans

---
+++ Java_intentos/src/java_intentos/Java_intentos.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_intentos;
+/**
+ *
+ * @author pc1
+ */
+public class Java_intentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("holaaaaa");
+        String nombre = "Atter ";
+        String apellido = "Basilio";
+        int numero = 79;
+        System.out.println(nombre + apellido);
+        System.out.println(numero);
+        int numero1 = 50 + 50;
+        int numero2 = numero1 + 50;
+        int numero3 = numero1 + numero2 + 50;
+        System.out.println(numero3);
+        int x = 10;
+        x += 5;
+        int a = 89;
+        a += 10;
+        System.out.println(x);
+        System.out.println(a);
+
+        
+    }
+    
+}
