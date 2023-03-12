8b6d54cdb08c81565d88d7b544be5406e67b613a
Atter12xd <1485275@senati.pe>
2023-03-11 19:16:40 -0500

    Declaramos la clase Scanner

--- Tarea_Java_Suma/Trabajo_Suma.java
+++ Tarea_Java_Suma/Trabajo_Suma.java
@@ -2,10 +2,12 @@
 package trabajo_suma;
 
 import java.util.Scanner; // Es una clase que nos permite obtener la entrada de datos primitivos
+
 public class Trabajo_Suma {
 
     public static void main(String[] args) {
-       
+        Scanner entrada = new Scanner(System.in); // Declaramos la clase "Scanner"
+
     }
-    
+
 }
