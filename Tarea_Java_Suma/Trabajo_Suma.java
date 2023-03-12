17aecc0850427c811a8e57ff417f705dee77ce7d
Atter12xd <1485275@senati.pe>
2023-03-11 19:11:21 -0500

    Importando la clase java.util.Scanner

---
+++ Tarea_Java_Suma/Trabajo_Suma.java
@@ -0,0 +1,11 @@
+
+package trabajo_suma;
+
+import java.util.Scanner; // Es una clase que nos permite obtener la entrada de datos primitivos
+public class Trabajo_Suma {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
