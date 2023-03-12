8df5f67021c70b9a73eb2e4e9f51523438a9bd8b
Atter12xd <1485275@senati.pe>
2023-03-11 19:18:52 -0500

    Pedimos al usario poner los numeros y ponemos una funcion nextInt

--- Tarea_Java_Suma/Trabajo_Suma.java
+++ Tarea_Java_Suma/Trabajo_Suma.java
@@ -7,6 +7,14 @@ public class Trabajo_Suma {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in); // Declaramos la clase "Scanner"
+        // Pedimos el primer numero
+        System.out.println("Ingrese el primer numero a sumar: ");
+        // "nextInt()" lee valores int introducidos por el usario
+        int numero1 = entrada.nextInt();
+        // pedimos el segundo numero
+        System.out.println("Ingrese el segundo numero a sumar: ");
+        // "nextInt()" lee valores int introducidos por el usario
+        int numero2 = entrada.nextInt();
 
     }
 
